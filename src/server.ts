6b2edece35cfdd7711aecc6f05731a08d6a54cb4
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-06-03 18:24:13 -0300

    Dia 2

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,17 @@
-import express from 'express';
+import express, { request } from 'express';
+import cors from 'cors';
+import path from 'path'
+import routes from './routes';
 
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    console.log('Listasgem de usuarios');
+app.use(cors());
+
+app.use(express.json());
+app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 
-
-    response.json([
-        'Diego',
-        "Cleiton",
-        "Robson",
-        "Danial"
-    ]);
-});
-
-app.listen(3333);
+app.listen(3333);  
